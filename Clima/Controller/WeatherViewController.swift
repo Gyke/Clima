9078f5a8c860441ab9cf4875c8e2e0b2e50c1420
Konstantin <don.kamatoz@bk.ru>
2023-02-25 20:00:40 +0500

    Немного рефакторинга

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,21 +8,26 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchButtonView: UIButton!
-    
-    @IBAction func searchButtonPressed(_ sender: UIButton) {
-        doSearch()
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         searchTextField.delegate = self
+    }
+ 
+}
+
+//MARK: - UITextFieldDelegate
+extension WeatherViewController: UITextFieldDelegate {
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        doSearch()
     }
     
     func textFieldDidChangeSelection(_ textField: UITextField) {
@@ -42,19 +47,6 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         return true
     }
     
-    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-        DispatchQueue.main.async {
-            self.temperatureLabel.text = weather.temperatureString
-            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
-            self.cityLabel.text = weather.cityName
-        }
-    }
-    
-    func didFailWithError(error: Error) {
-        //                TODO: Всплывающее окно, где будет сказано, что название города введено неправильно
-        print(error)
-    }
-    
     func doSearch() {
         searchTextField.endEditing(true)
         let appid = "6d2d8b70eee2e0fb60d968d3d7de6392"
@@ -68,3 +60,18 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
 
 }
 
+// MARK: - WeatherManagerDelegate
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        //                TODO: Всплывающее окно, где будет сказано, что название города введено неправильно
+        print(error)
+    }
+}
